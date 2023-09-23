ef8d6cf28729decf16abd09d9ec4e9cb5b37ac71
Dmitry Medvedev <medved-dmi@yandex.ru>
2023-09-23 22:13:14 +0300

    Done homework

---
+++ task 41/Program.cs
@@ -0,0 +1,79 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+// int[] numbers = new int[5];
+// Fillarrayrandomnum(numbers);
+// PrintArray(numbers);
+// Console.WriteLine($"Количество чисел больше 0 - {Positivenumbers(numbers)}");
+
+// void Fillarrayrandomnum(int[] array)
+// {
+//     Random rnd = new Random();
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = rnd.Next();
+//     }
+// }
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+// }
+// int Positivenumbers(int[] array)
+// {
+//     int count = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] > 0)
+//         {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+
+string[] array = WorkWithUser("Введите числа через запятую: ");
+int[] collection = DigitOfString(array);
+Console.WriteLine(CountPositive(collection));
+
+
+string[] WorkWithUser(string message)
+{
+Console.Write(message);
+string str = Console.ReadLine();
+string[] symb = str.Split(", ");
+return symb;
+}
+int[] DigitOfString(string[] symbs)// Преобразует строковый массив в целочисленный
+{
+int[] digits = new int[symbs.Length];
+for (int i = 0; i < digits.Length; i++)
+{
+digits[i] = int.Parse(symbs[i]);
+}
+return digits;
+}
+int CountPositive(int[] arr)
+{
+int res = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+if(arr[i] > 0) res++;
+}
+return res;
+}
+
+void PrintArray(int[] array)//Выводит массив на печать
+{
+for (int i = 0; i < array.Length - 1; i++)
+{
+Console.Write(array[i] + ", ");
+}
+Console.WriteLine(array[array.Length - 1] + "]");
+}
---
+++ task 43/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double k1 = WorkWithUser("Введите значение k1: ");
+double k2 = WorkWithUser("Введите значение k2: ");
+double b1 = WorkWithUser("Введите значение b1: ");
+double b2 = WorkWithUser("Введите значение b2: ");
+double[] res = Resolve(k1, b1, k2, b2);
+Console.WriteLine(string.Join(", ", res));
+
+double WorkWithUser(string msg)
+{
+    Console.WriteLine(msg);
+    double number = double.Parse(Console.ReadLine());
+    return number;
+}
+
+double[] Resolve(double k1, double b1, double k2, double b2)
+{
+    double[] resolve = new double[2];
+    resolve[0] = (b2 - b1) / (k1 - k2);
+    resolve[1] = k1 * resolve[0] + b1;
+    return resolve;
+}
